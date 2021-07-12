f165152513481df7c75d51c70b3904e92f1a52bb
Itsusinn <itsusinn@foxmail.com>
2021-07-12 15:30:18 +0800

    feat(time): 支持获取delta time

---
+++ src/time.rs
@@ -0,0 +1,19 @@
+use std::time::Instant;
+use std::sync::RwLock;
+
+lazy_static!{
+    static ref PREVIOUS_TIME:RwLock<f32> = RwLock::new(0.0);
+    static ref DELTA_TIME:RwLock<f32> = RwLock::new(0.0);
+    static ref START:Instant = Instant::now();
+}
+
+pub fn update(){
+    let now = START.elapsed().as_secs_f32();
+    let mut pervious = PREVIOUS_TIME.write().unwrap();
+    let mut delta = DELTA_TIME.write().unwrap();
+    *delta = now - *pervious;
+    *pervious = now;
+}
+pub fn get_delta() -> f32{
+    *DELTA_TIME.read().unwrap()
+}
